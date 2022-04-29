c96c0ac38047d988497cc6b242cc0f072aa92c3a
김응길 <yesroad.dev@gmail.com>
2022-04-29 15:29:43 +0900

    emit 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': [
+			'warn',
+			{
+				singleQuote: true,
+				parser: 'typescript',
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+			},
+		],
+	},
 };
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<label for="todo-input"></label>
+		<input id="todo-input" type="text" :value="item" @input="handleInput" />
+		<button v-on:click="addTodo" type="button">add</button>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	props: ['item'],
+	methods: {
+		handleInput(e: any) {
+			this.$emit('input', e.target.value);
+		},
+		addTodo() {
+			console.log('add');
+		},
+	},
+});
+</script>
+
+<style scoped></style>
