e5f577ab54e43744f0d16a8c3ec3dda32e87fc36
김응길 <yesroad.dev@gmail.com>
2022-05-07 16:15:56 +0900

    타입스크립트 프로젝트 신규 생성

---
+++ vue-news-ts/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+		'@vue/typescript/recommended',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'prettier/prettier': [
+			'warn',
+			{
+				singleQuote: true,
+				parser: 'typescript',
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+			},
+		],
+	},
+};
